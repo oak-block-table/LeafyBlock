a5797048506f169c6c1d8da8d362b43ea203e055
Oak Table <none>
2021-08-20 09:09:14 -0700

    Work-in-progress: Try to drop a diamond item

--- src/main/java/org/hexagonalmagmacube/LeafyBlock/LeavesBlockBreakEventListener.java
+++ src/main/java/org/hexagonalmagmacube/LeafyBlock/LeavesBlockBreakEventListener.java
@@ -2,11 +2,13 @@ package org.hexagonalmagmacube.LeafyBlock;
 
 import java.util.Random;
 
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 
 import static org.bukkit.Bukkit.getServer;
 
@@ -17,8 +19,8 @@ public class LeavesBlockBreakEventListener implements Listener {
     @EventHandler
     public void onEvent(BlockBreakEvent event) {
         int dieRoll = rand.nextInt(20);
+        Player player = event.getPlayer();
         if (dieRoll == 0) {
-            Player player = event.getPlayer();
             getServer().broadcastMessage(String.format("Natural 0. Watch out %s.", player.getDisplayName()));
             // player.setHealth(0);
             // event.getPlayer().kickPlayer("Natural zero.");
@@ -28,15 +30,23 @@ public class LeavesBlockBreakEventListener implements Listener {
         }
         if (dieRoll == 20) {
             getServer().broadcastMessage(String.format("Twenty is awesome.", dieRoll));
+            return;
         }
 
-        if (dieRoll % 2 == 1) {
-            getServer().broadcastMessage(String.format("%d rolled. Good event,", dieRoll));
+        if (dieRoll == 18) {
+            Block block = event.getBlock();
+            getServer().broadcastMessage(String.format("Drop a diamond.", dieRoll));
+            //player.getWorld().dropItemNaturally(new ItemStack());
+            return;
         }
-        else
-        {
-            getServer().broadcastMessage(String.format("%d rolled. Bad event,", dieRoll));
-        }
+
+//        if (dieRoll % 2 == 1) {
+//            getServer().broadcastMessage(String.format("%d rolled. Good event,", dieRoll));
+//        }
+//        else
+//        {
+//            getServer().broadcastMessage(String.format("%d rolled. Bad event,", dieRoll));
+//        }
     }
 
 }
